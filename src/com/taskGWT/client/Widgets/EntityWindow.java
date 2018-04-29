c676c787d4981dbfc9cbd58d16c323a25fa80c2d
aleksandrfomin <fsasha_1997@mail.ru>
2018-04-29 23:57:33 +0300

    main widgets

---
+++ src/com/taskGWT/client/Widgets/EntityWindow.java
@@ -0,0 +1,50 @@
+package com.taskGWT.client.Widgets;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HTMLPanel;
+import com.google.gwt.user.client.ui.IsWidget;
+import com.google.gwt.user.client.ui.TextBox;
+import com.taskGWT.client.Entities.Person;
+
+public class EntityWindow extends Composite implements IsWidget {
+    interface EntityWindowUiBinder extends UiBinder<HTMLPanel, EntityWindow> {
+    }
+
+    private static EntityWindowUiBinder ourUiBinder = GWT.create(EntityWindowUiBinder.class);
+
+    private ItemsList itemsList;
+
+    private Person person;
+
+    @UiField
+    TextBox name;
+    @UiField
+    TextBox lastName;
+
+    public EntityWindow(ItemsList itemsList) {
+        initWidget(ourUiBinder.createAndBindUi(this));
+        setVisible(false);
+        this.itemsList = itemsList;
+    }
+
+    public void showEntity(Person person){
+        setVisible(true);
+        this.person = person;
+        name.setText(person.getName());
+        lastName.setText(person.getLastName());
+    }
+
+    @UiHandler("update")
+    public void updateClick(ClickEvent event) {
+        int index = itemsList.getData().indexOf(person);
+        itemsList.getData().get(index).setName(name.getText());
+        itemsList.getData().get(index).setLastName(lastName.getText());
+        itemsList.getDataProvider().refresh();
+        setVisible(false);
+    }
+}
--- src/com/taskGWT/client/Widgets/ItemsList.java
+++ src/com/taskGWT/client/Widgets/ItemsList.java
@@ -2,36 +2,83 @@ package com.taskGWT.client.Widgets;
 
 import com.google.gwt.cell.client.TextCell;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.DivElement;
 import com.google.gwt.dom.client.LabelElement;
+import com.google.gwt.event.dom.client.DoubleClickEvent;
+import com.google.gwt.event.dom.client.DoubleClickHandler;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.cellview.client.CellList;
-import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.HTMLPanel;
-import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.*;
+import com.google.gwt.view.client.ListDataProvider;
+import com.google.gwt.view.client.SingleSelectionModel;
 import com.taskGWT.client.Entities.Person;
 
 import java.util.Arrays;
 import java.util.List;
 
-public class ItemsList extends Composite {
+public class ItemsList extends Composite implements IsWidget {
     interface ItemsListUiBinder extends UiBinder<HTMLPanel, ItemsList> {
     }
 
     private static ItemsListUiBinder ourUiBinder = GWT.create(ItemsListUiBinder.class);
 
     private static final List<Person> PEOPLE = Arrays.asList(
-            new Person("Aleksandr","Fomin"),
+            new Person("Aleksandr", "Fomin"),
             new Person("Roman", "Fomin"));
 
-    private PersonCell personCell = new PersonCell();
+    private PersonCell personCell;
+
+    private CellList<Person> cellList;
+
+    public List<Person> getData() {
+        return data;
+    }
+
+    public ListDataProvider<Person> getDataProvider() {
+        return dataProvider;
+    }
+
+    private List<Person> data;
+
+    private ListDataProvider<Person> dataProvider;
+
+    @UiField
+    HTMLPanel listOfEntities;
+    @UiField
+    HTMLPanel entWindow;
 
     public ItemsList() {
         initWidget(ourUiBinder.createAndBindUi(this));
-        CellList<Person> cellList = new CellList<Person>(personCell);
-        cellList.setRowCount(PEOPLE.size(),true);
-        cellList.setRowData(0,PEOPLE);
-        RootPanel.get().add(cellList);
+        initEntityList();
     }
+
+    private void initEntityList() {
+        personCell = new PersonCell();
+
+        cellList = new CellList<>(personCell);
+
+        EntityWindow entityWindow = new EntityWindow(this);
+
+        dataProvider = new ListDataProvider<>();
+        dataProvider.addDataDisplay(cellList);
+
+        data = dataProvider.getList();
+        for (Person p : PEOPLE) {
+            data.add(p);
+        }
+
+        SingleSelectionModel<Person> selectionModel = new SingleSelectionModel<>();
+        cellList.setSelectionModel(selectionModel);
+
+        cellList.addDomHandler(event -> {
+            Person selected = selectionModel.getSelectedObject();
+            entityWindow.showEntity(selected);
+        }, DoubleClickEvent.getType());
+
+        listOfEntities.add(cellList);
+        entWindow.add(entityWindow);
+    }
+
 }
